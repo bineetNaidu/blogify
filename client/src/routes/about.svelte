f0a93a9aab21a3f86e51a62aa6bfc0200c0cdc99
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-12 10:57:50 +0530

    add about page && updated api settings

--- client/src/routes/about.svelte
+++ client/src/routes/about.svelte
@@ -1,7 +1,20 @@
 <svelte:head>
-	<title>About</title>
+  <title>Blogify | About</title>
 </svelte:head>
 
 <h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>
+  BLOGIFY is a community of software developers getting together to help one
+  another out. The software industry relies on collaboration and networked
+  learning. We provide a place for that to happen.
+</p>
+<p>
+  Our application is open source, meaning you can inspect every little detail of
+  the code, or chip in yourself! We are working to make our platform available
+  for anyone to stand up similar communities in any niche or passion.
+</p>
+<p>
+  We believe in transparency and adding value to the ecosystem. We hope you poke
+  around and like what you see!
+</p>
