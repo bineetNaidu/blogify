14246ff1a22552b16185647fea63cd072b1d0d9e
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-12 15:17:04 +0530

    add Footer

---
+++ client/src/components/Footer.svelte
@@ -0,0 +1,11 @@
+<style>
+  footer {
+    border-top: 1px solid rgba(255, 62, 0, 0.1);
+    text-align: center;
+    padding: 5px 0;
+  }
+</style>
+
+<footer>
+  <p>Blogify @2020. Copyright Reserved</p>
+</footer>
